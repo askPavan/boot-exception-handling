ec5155229c505ad051d290afeb82823f88f4dc2a
askpavan <askpavanit@gmail.com>
2021-07-03 14:41:49 +0530

    git initial checkin.

---
+++ src/main/java/com/example/demo/BootExceptionHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootExceptionHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootExceptionHandlingApplication.class, args);
+	}
+
+}
